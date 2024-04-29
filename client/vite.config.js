b6c2578a377119b18bbfe94bbf94f7aa15509beb
Alita1093 <furkanozturk1635@gmail.com>
2024-04-29 13:02:41 +0300

    Remove vercel.json and update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const ClientSideRouting = {
-  name: "dynamic-router",
-  configureServer(server) {
-    server.middlewares.use((req, res, next) => {
-      if (req.url.search(/^\/@\d+/) !== -1) {
-        req.url = "/";
-      }
-      next();
-    });
-  },
-};
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 10000,
-    open: true,
-  },
 });
