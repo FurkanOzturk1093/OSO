ad62bf8d6c1e03c6e4a1a3f9f41d5273f2d7d90f
Alita1093 <furkanozturk1635@gmail.com>
2024-04-29 11:42:01 +0300

    Add client-side routing and configure server

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const ClientSideRouting = {
+  name: "dynamic-router",
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      if (req.url.search(/^\/@\d+/) !== -1) {
+        req.url = "/";
+      }
+      next();
+    });
+  },
+};
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000,
+    open: true,
+  },
 });
